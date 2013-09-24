be9537cafeca976fca277aaf524656812d149950
Jim Berlage + Sterling Forest <sterling.kxz@gmail.com>
2013-09-24 10:38:45 -0400

    finishes curry functions

--- exercises.js
+++ exercises.js
@@ -1,5 +1,5 @@
 var sum = function() {
-  var result = 0;
+  var result = this.age;
 
   for (var i = 0; i < arguments.length; i++) {
     result += arguments[i];
@@ -8,4 +8,72 @@ var sum = function() {
   return result;
 }
 
-console.log(sum(1,2,3,4));
+Function.prototype.myBind = function(obj) {
+  var that = this;
+
+  otherArgs = [];
+  for (var i = 1; i < arguments.length; i++) {
+    otherArgs.push(arguments[i]);
+  }
+
+  var result = function() {
+    var newArgs = otherArgs;
+    for (var j = 0; j < arguments.length; j++) {
+      newArgs.push(arguments[j]);
+    }
+    return that.apply(obj, newArgs);
+  };
+  return result;
+}
+
+
+function Cat (age) {
+  this.age = age;
+}
+
+// test(1, 5);
+
+c = new Cat(5);
+
+// var newSum = sum.myBind(c, 3, 4);
+//
+// console.log(newSum(2));
+
+Function.prototype.curry = function() {
+  var that = this;
+  var args = [];
+
+  for (var i = 0; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+
+  var result = function() {
+    var newArgs = args;
+    for (var j = 0; j < arguments.length; j++) {
+      newArgs.push(arguments[j]);
+    }
+    return that.apply(that, newArgs);
+  }
+  return result;
+}
+
+var curriedSum = function(threshold) {
+  var result = function() {
+    if (arguments.length >= threshold) {
+      var sum = 0;
+      for (var i = 0; i < threshold; i++) {
+        sum += arguments[i];
+      }
+      return sum;
+    } else {
+      return result.curry.apply(result, arguments);
+    }
+  }
+  return result;
+}
+
+var currySum = curriedSum(4);
+console.log( currySum(5)(2)(1)(6) );
+
+
+
